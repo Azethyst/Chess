4c126e519a02860c02ae317f1ce8a82cddd9363e
Brix <112671061+Azethyst@users.noreply.github.com>
2023-01-18 12:17:51 -0600

    added id's and javascript file
    added id's to every square in the html file and added the initial javascript file.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const players = ["white", "black"];
+var turn = 0;  // 0 = white, 1 = black
+
+const square = document.querySelectorAll(".square");
+window.addEventListener("click", () => {
+    console.log(square);
+});
