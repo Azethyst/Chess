5b230b50fe46d204d524fd4d75fcf99e0f816d1e
Brix <112671061+Azethyst@users.noreply.github.com>
2023-01-19 14:22:13 -0600

    added some functionality in js
    added hover functionality and click functionality in chess

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 const players = ["white", "black"];
 var turn = 0;  // 0 = white, 1 = black
 
-const square = document.querySelectorAll(".square");
-window.addEventListener("click", () => {
-    console.log(square);
-});
+const board = document.querySelector(".board");
+const iconList = board.querySelectorAll(".black,.white");
+const iconTotalNumn = iconList.length;
+const squareList = board.querySelectorAll(".square");
+const squareTotalNum = squareList.length;
+var lastSelection;  // the square currently selected
+var init = 1;
+
+for (let i = 0; i < squareTotalNum; i++ ) {
+    const a = squareList[i].querySelector("i");
+    a.addEventListener("click", function () {
+        if (init == 0) {
+            lastSelection.classList.remove("selected");
+        } else {
+            init = 0;
+        }
+        this.classList.toggle("selected");
+        lastSelection = this;
+    })
+}
+
