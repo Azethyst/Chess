db45c7cd47cb73b45c59db6ee6404d2f7402c21e
Brix Ian Marcellana <marcellana_brix@yahoo.com>
2023-01-19 19:02:47 -0600

    Fixed the click and hover functionality
    fixed the issue where border does not pop up on hover and click was not too responsive.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 const players = ["white", "black"];
-var turn = 0;  // 0 = white, 1 = black
+var turn = 0; // 0 = white, 1 = black
 
 const board = document.querySelector(".board");
 const iconList = board.querySelectorAll(".black,.white");
 const iconTotalNumn = iconList.length;
 const squareList = board.querySelectorAll(".square");
 const squareTotalNum = squareList.length;
-var lastSelection;  // the square currently selected
+var lastSelection; // the square currently selected
 var init = 1;
 
-for (let i = 0; i < squareTotalNum; i++ ) {
-    const a = squareList[i].querySelector("i");
-    a.addEventListener("click", function () {
+for (let i = 0; i < squareTotalNum; i++) {
+  const a = squareList[i];
+  a.addEventListener("click", function () {
+    if (this.classList.contains("black") || this.classList.contains("white")) {
+      if (this == lastSelection) {
+        init = 1;
+        this.classList.remove("selected");
+        lastSelection = null;
+      } else {
         if (init == 0) {
-            lastSelection.classList.remove("selected");
+          lastSelection.classList.remove("selected");
         } else {
-            init = 0;
+          init = 0;
         }
         this.classList.toggle("selected");
         lastSelection = this;
-    })
+      }
+    }
+  });
 }
-
