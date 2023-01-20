5b0f0a11265322ace734507627ba712b1b58306e
Brix <112671061+Azethyst@users.noreply.github.com>
2023-01-20 17:31:24 -0600

    completed movement and eat functionality of the pieces
    need to implement:
    - king check
    - castle
    - en passant
    - promotion
    - score counting system

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const players = ["white", "black"];
-var turn = 0; // 0 = white, 1 = black
+var turn = false; // false = white, true = black
 
 const board = document.querySelector(".board");
 const squareList = board.querySelectorAll(".square");
@@ -11,15 +11,19 @@ var init = 1;
 for (let i = 0; i < squareTotalNum; i++) {
   const a = squareList[i];
   a.addEventListener("click", function () {
-    turnControl(this);
+    if (a.classList.contains("moves")) {
+      movePiece(this);
+    } else {
+      turnControl(this);
+    }
   });
 }
 
 /** controls the turn based on the turn global variable */
 function turnControl(square) {
-  if (turn == 0 && square.classList.contains("white")) {
+  if (!turn && square.classList.contains("white")) {
     selectorControl(square, "white");
-  } else if (turn == 1 && square.classList.contains("black")) {
+  } else if (turn && square.classList.contains("black")) {
     selectorControl(square, "black");
   }
 }
@@ -48,27 +52,49 @@ function selectorControl(square, color) {
 function pieceMovement(square, color) {
   const piece = square.querySelector("i").classList[1].substring(9);
   console.log(piece);
-  if (piece == "pawn" && color == "black") {
+  if (piece == "pawn" && color == "black") {                 // white pawn
     const id = square.id;
     const left_id = +id[0] - 1 + " " + (+id[2] - 1);
     const right_id = +id[0] + 1 + " " + (+id[2] - 1);
-    if (isValidSquare(left_id)) {
+    const forward_id = +id[0] + " " + (+id[2] - 1);
+    const jump_id = +id[0] + " " + (+id[2] - 2);
+    if (isValidSquare(forward_id) && !hasColor(forward_id, "black") && !hasColor(forward_id, "white")) {
+      document.getElementById(forward_id).classList.toggle("moves");
+
+      if (id[2] == "7") {
+        if (isValidSquare(jump_id) && !hasColor(jump_id, "white") && !hasColor(jump_id, "black")) {
+          document.getElementById(jump_id).classList.toggle("moves");
+        }
+      }
+    }
+    if (isValidSquare(left_id) && hasColor(left_id,"white")) {
       document.getElementById(left_id).classList.toggle("moves");
     }
-    if (isValidSquare(right_id)) {
+    if (isValidSquare(right_id) && hasColor(right_id,"white")) {
       document.getElementById(right_id).classList.toggle("moves");
     }
-  } else if (piece == "pawn" && color == "white") {
+  } else if (piece == "pawn" && color == "white") {         // black pawn
     const id = square.id;
     const left_id = +id[0] - 1 + " " + (+id[2] + 1);
     const right_id = +id[0] + 1 + " " + (+id[2] + 1);
-    if (isValidSquare(left_id)) {
+    const forward_id = +id[0] + " " + (+id[2] + 1);
+    const jump_id = +id[0] + " " + (+id[2] + 2);
+    if (isValidSquare(forward_id) && !hasColor(forward_id, "white") && !hasColor(forward_id, "black")) {
+      document.getElementById(forward_id).classList.toggle("moves");
+
+      if (id[2] == "2") {
+        if (isValidSquare(jump_id) && !hasColor(jump_id, "black") && !hasColor(jump_id, "white")) {
+          document.getElementById(jump_id).classList.toggle("moves");
+        }
+      }
+    }
+    if (isValidSquare(left_id) && hasColor(left_id,"black")) {
       document.getElementById(left_id).classList.toggle("moves");
     }
-    if (isValidSquare(right_id)) {
+    if (isValidSquare(right_id) && hasColor(right_id,"black")) {
       document.getElementById(right_id).classList.toggle("moves");
     }
-  } else if (piece == "knight") {
+  } else if (piece == "knight") {                            // knight
     const id = square.id;
     const ids = [
       +id[0] + 1 + " " + (+id[2] + 2),
@@ -80,12 +106,18 @@ function pieceMovement(square, color) {
       +id[0] - 2 + " " + (+id[2] + 1),
       +id[0] - 1 + " " + (+id[2] + 2),
     ];
-    for (let i = 0; i < ids.length; i++) {
-      if (isValidSquare(ids[i])) {
-        document.getElementById(ids[i]).classList.toggle("moves");
-      }
+    for (let i = 0; i < ids.length; i++) {        
+      if (color == "black") {
+        if (isValidSquare(ids[i]) && !hasColor(ids[i], "black")) {
+          document.getElementById(ids[i]).classList.toggle("moves");
+        }
+      } else if (color == "white") {
+        if (isValidSquare(ids[i]) && !hasColor(ids[i], "white")) {
+          document.getElementById(ids[i]).classList.toggle("moves");
+        }
+      }   
     }
-  } else if (piece == "bishop") {
+  } else if (piece == "bishop") {                            // bishop
     for (let i = 0; i < 4; i++) {
       var en_seq = true;
       var id = square.id;
@@ -99,14 +131,28 @@ function pieceMovement(square, color) {
         } else {
           id = +id[0] - 1 + " " + (+id[2] + 1);
         }
-        if (isValidSquare(id)) {
-          document.getElementById(id).classList.toggle("moves");
-        } else {
-          en_seq = false;
+        if (color == "black") {
+          if (isValidSquare(id) && !hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
+        } else if (color == "white") {
+          if (isValidSquare(id) && !hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
         }
       }
     }
-  } else if (piece == "rook") {
+  } else if (piece == "rook") {                              // rook
     for (let i = 0; i < 4; i++) {
       var en_seq = true;
       var id = square.id;
@@ -120,14 +166,28 @@ function pieceMovement(square, color) {
         } else {
           id = +id[0] - 1 + " " + +id[2];
         }
-        if (isValidSquare(id)) {
-          document.getElementById(id).classList.toggle("moves");
-        } else {
-          en_seq = false;
+        if (color == "black") {
+          if (isValidSquare(id) && !hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
+        } else if (color == "white") {
+          if (isValidSquare(id) && !hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
         }
       }
     }
-  } else if (piece == "king") {
+  } else if (piece == "king") {                             // king
     const id = square.id;
     const ids = [
       +id[0] + 1 + " " + +id[2],
@@ -140,11 +200,17 @@ function pieceMovement(square, color) {
       +id[0] + 1 + " " + (+id[2] - 1),
     ];
     for (let i = 0; i < ids.length; i++) {
-      if (isValidSquare(ids[i])) {
-        document.getElementById(ids[i]).classList.toggle("moves");
-      }
+      if (color == "black") {
+        if (isValidSquare(ids[i]) && !hasColor(ids[i], "black")) {
+          document.getElementById(ids[i]).classList.toggle("moves");
+        }
+      } else if (color == "white") {
+        if (isValidSquare(ids[i]) && !hasColor(ids[i], "white")) {
+          document.getElementById(ids[i]).classList.toggle("moves");
+        }
+      }   
     }
-  } else if (piece == "queen") {
+  } else if (piece == "queen") {                             // queen
     // N, S, W, E
     for (let i = 0; i < 4; i++) {
       var en_seq = true;
@@ -159,10 +225,24 @@ function pieceMovement(square, color) {
         } else {
           id = +id[0] - 1 + " " + +id[2];
         }
-        if (isValidSquare(id)) {
-          document.getElementById(id).classList.toggle("moves");
-        } else {
-          en_seq = false;
+        if (color == "black") {
+          if (isValidSquare(id) && !hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
+        } else if (color == "white") {
+          if (isValidSquare(id) && !hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
         }
       }
     }
@@ -180,15 +260,30 @@ function pieceMovement(square, color) {
         } else {
           id = +id[0] - 1 + " " + (+id[2] + 1);
         }
-        if (isValidSquare(id)) {
-          document.getElementById(id).classList.toggle("moves");
-        } else {
-          en_seq = false;
+        if (color == "black") {
+          if (isValidSquare(id) && !hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
+        } else if (color == "white") {
+          if (isValidSquare(id) && !hasColor(id, "white")) {
+            document.getElementById(id).classList.toggle("moves");
+          } else if (isValidSquare(id) && hasColor(id, "black")) {
+            document.getElementById(id).classList.toggle("moves");
+            en_seq = false;
+          } else {
+            en_seq = false;
+          }
         }
       }
     }
   }
 }
+
 
 /** removes all the "moves" class of all the squares in the board */
 function removeMoves() {
@@ -210,5 +305,45 @@ function isValidSquare(id) {
     }
   } else {
     return false;
+  }
+}
+
+/** checks whether the square has the specified color */
+function hasColor(id, color) {
+  if (document.getElementById(id).classList.contains(color)) {
+    return true;
+  }
+  return false;
+}
+
+/** lets the user move piece on the board */
+function movePiece(square) {
+  removeMoves();
+  lastSelection.classList.remove("selected");
+
+  // remove the piece thats being eaten if the piece exists
+  if (square.classList.contains("black") || square.classList.contains("white")) {
+    square.classList.remove(square.classList[2]);
+    square.querySelector("i").classList.remove(square.querySelector("i").classList[1]);
+  }
+
+  // shift th piece to the targeted square
+  square.classList.add(lastSelection.classList[2]);
+  square.querySelector("i").classList.add(lastSelection.querySelector("i").classList[1]);
+
+  // remove the piece from its original square
+  lastSelection.classList.remove(lastSelection.classList[2]);
+  lastSelection.querySelector("i").classList.remove(lastSelection.querySelector("i").classList[1]);
+
+  lastSelection = null;
+  init = 1;
+
+  turn = !turn;
+  if (turn) {
+    document.getElementById("b-span").classList.remove("hidden");
+    document.getElementById("w-span").classList.add("hidden");
+  } else {
+    document.getElementById("w-span").classList.remove("hidden");
+    document.getElementById("b-span").classList.add("hidden");
   }
 }
